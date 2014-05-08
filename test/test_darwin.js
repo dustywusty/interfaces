6b9826bf92c4cb9445c2661a1deb54459ef1afd7
clarkda <dusty@clarkda.com>
2014-05-07 22:38:10 -0500

    adding darwin tests for en0 and lo0

---
+++ test/test_darwin.js
@@ -0,0 +1,64 @@
+var _           = require('underscore')
+  , assert      = require('assert')
+  , interfaces  = require('..')
+  , mocha       = require('mocha')
+  , mockery     = require('mockery');
+
+mockery.enable({
+  useCleanCache: true,
+  warnOnReplace: false,
+  warnOnUnregistered: false
+});
+
+
+/**
+ * TEST DARWIN INTERFACE en0
+ */
+
+describe('interface() - darwin', function() {
+
+  var config = {
+    interface: 'en0',
+    platform: 'darwin',
+    macAddress:'b8:f6:b1:1c:2e:07'
+  };
+
+  mockery.registerMock('config', config);
+  mockery.registerMock('os', require('./mock/os.js'));
+  mockery.registerMock('./lib/utils', require('./mock/lib-utils.js'));
+
+  _.each(interfaces()[config.interface], function(address) {
+    it('- mac address should equal ' + config.macAddress, function() {
+      assert.equal(config.macAddress, address.mac);
+    });
+  });
+
+  mockery.deregisterMock('os');
+  mockery.deregisterMock('./lib/utils');
+});
+
+/**
+ * TEST DARWIN INTERFACE lo0
+ */
+
+describe('interface() - darwin', function() {
+
+  var config = {
+    interface: 'lo0',
+    platform: 'darwin',
+    macAddress:'00:00:00:00:00:00'
+  };
+
+  mockery.registerMock('config', config);
+  mockery.registerMock('os', require('./mock/os.js'));
+  mockery.registerMock('./lib/utils', require('./mock/lib-utils.js'));
+
+  _.each(interfaces()[config.interface], function(address) {
+    it('- mac address should equal ' + config.macAddress, function() {
+      assert.equal(config.macAddress, address.mac);
+    });
+  });
+
+  mockery.deregisterMock('os');
+  mockery.deregisterMock('./lib/utils');
+});
